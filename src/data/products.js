596eeafd12470c03e7fec1b089a643f8dc95fead
Javoc <javier.ochoa.77@gmail.com>
2024-11-25 11:45:07 -0300

    feat: :zap: Crea archivo de productos.

---
+++ src/data/products.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      name: "Base para relleno",
+      price: 4990,
+      image: "https://images.unsplash.com/photo-1701884781051-ee7d500c8696?w=600"
+    },
+    {
+      id: 2,
+      name: "Pack de 8 macarons",
+      price: 7990,
+      image: "https://images.unsplash.com/photo-1702234670259-4424c995a451?w=600"
+    },
+    {
+      id: 3,
+      name: "Pack de 12 macarons",
+      price: 12990,
+      image: "https://images.unsplash.com/photo-1652555286866-1bef20014bc0?w=600"
+    },
+    {
+      id: 4,
+      name: "Relleno especial",
+      price: 5990,
+      image: "https://images.unsplash.com/photo-1689618255209-c55d7a96dde6?w=600"
+    },
+    {
+      id: 5,
+      name: "Macarons personalizados",
+      price: 9990,
+      image: "https://images.unsplash.com/photo-1697378594766-e22f529d7b36?w=600"
+    }
+  ];
